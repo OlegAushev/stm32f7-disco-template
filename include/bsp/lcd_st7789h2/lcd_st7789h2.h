b9318e78e1b6d85fcacc79313e1525087b55893b
Oleg Aushev <aushevom@gmail.com>
2022-08-17 23:39:58 +0300

    add disco lcd class

---
+++ include/bsp/lcd_st7789h2/lcd_st7789h2.h
@@ -0,0 +1,67 @@
+/**
+ * @file lcd_st7789h2.h
+ * @author Oleg Aushev (aushevom@protonmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-08-17
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+
+#pragma once
+
+
+#include "stm32f723e_discovery_lcd.h"
+#include "mcu_stm32f7/system/mcu_system.h"
+#include <cstring>
+
+
+namespace bsp
+{
+
+
+class LCD_st7789h2
+{
+private:
+	LCD_st7789h2(sFONT* font);
+	LCD_st7789h2(const LCD_st7789h2& other) = delete;
+	LCD_st7789h2& operator=(const LCD_st7789h2& other) = delete;
+
+	static LCD_st7789h2& instance_impl(sFONT* font = nullptr)
+	{
+		static LCD_st7789h2 s_instance(font);
+		return s_instance;
+	}
+
+public:
+	static void init(sFONT* font)
+	{
+		instance_impl(font);
+	}
+
+	static LCD_st7789h2& instance()
+	{
+		return instance_impl();
+	}
+
+	void print(uint16_t line, const char* str)
+	{
+		char buf[32];
+		strncpy(buf, str, 32);
+		BSP_LCD_DisplayStringAtLine(line, reinterpret_cast<uint8_t*>(buf));
+	}
+
+	void clearLine(uint16_t line)
+	{
+		BSP_LCD_ClearStringLine(line);
+	}
+
+};
+
+
+} // namespace bsp
+
+
+
--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,8 @@
 #include "stm32f723e_discovery.h"
 #include "mcu_stm32f7/system/mcu_system.h"
 
+#include "bsp/lcd_st7789h2/lcd_st7789h2.h"
+
 
 void MX_GPIO_Init();
 
@@ -24,16 +26,13 @@ int main()
 	mcu::initSystemClock();
 	HAL_Delay(500);
 	MX_GPIO_Init();
-
-	float a = 1.f;
-	float b = 42.2f;
-	volatile float c = a + b;
-
-	HAL_GPIO_TogglePin(LED6_GPIO_PORT, LED6_PIN);
 	
+	bsp::LCD_st7789h2::init(&Font12);
+	bsp::LCD_st7789h2::instance().print(0, "Hello world!");
+
 	while (1)
 	{
-		c += 1;
+
 		HAL_GPIO_TogglePin(LED5_GPIO_PORT, LED5_PIN);
 		HAL_GPIO_TogglePin(LED6_GPIO_PORT, LED6_PIN);
 		
@@ -42,8 +41,13 @@ int main()
 }
 
 
-
+/**
+ * @brief Mandatory HAL-lib tick handler.
+ * 
+ */
 extern "C" void SysTick_Handler(void)
 {
 	HAL_IncTick();
 }
+
+
