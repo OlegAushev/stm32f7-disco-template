9135c19011787f2b5a9a04fb57618bd82305a235
Oleg Aushev <aushevom@gmail.com>
2022-10-09 22:56:12 +0300

    restyle

--- include/bsp_f723_disco/leds/leds.cpp
+++ include/bsp_f723_disco/leds/leds.cpp
@@ -17,23 +17,23 @@ namespace bsp {
 
 
 mcu::GpioOutput ledRed;
-const mcu::GpioConfig ledRedConfig = {.port = LED5_GPIO_PORT,
-				.pin = {	.Pin = LED5_PIN,
+const mcu::GpioConfig ledRedConfig = {	.port = LED5_GPIO_PORT,
+					.pin = {.Pin = LED5_PIN,
 						.Mode = GPIO_MODE_OUTPUT_PP,
 						.Pull = GPIO_NOPULL,
 						.Speed = GPIO_SPEED_FREQ_LOW,
 						.Alternate = 0},
-				.activeState = emb::PinActiveState::HIGH};
+					.activeState = emb::PinActiveState::HIGH};
 
 
 mcu::GpioOutput ledGreen;
 const mcu::GpioConfig ledGreenConfig = {.port = LED6_GPIO_PORT,
-				.pin = {	.Pin = LED6_PIN,
+					.pin = {.Pin = LED6_PIN,
 						.Mode = GPIO_MODE_OUTPUT_PP,
 						.Pull = GPIO_NOPULL,
 						.Speed = GPIO_SPEED_FREQ_LOW,
 						.Alternate = 0},
-				.activeState = emb::PinActiveState::HIGH};
+					.activeState = emb::PinActiveState::HIGH};
 				
 
 void initLedRed()
